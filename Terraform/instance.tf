71df5c4f3445f0034e509fd949973073e93d1311
aimanmubasher <24100312@lums.edu.pk>
2024-07-10 15:57:31 +0500

    adding running terraform script

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -1,30 +1,40 @@
-provider "aws" 
-{  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.57.0"
+    }
+  }
+}
+terraform{
+    backend "s3"{
+        bucket = "devops-assignment-s3-bucket"
+        key = "env/dev/terraform.tfstate"
+        region = "us-east-1"
+    }
+}
+
+provider "aws" {  
     region = "us-east-1"
-    access_key = ""
-    secret_key = ""
 }
     
-resource "aws_vpc" "devops_vpc" 
-{    
-    cidr_block = "20.0.0.0/32"
+resource "aws_vpc" "devops_vpc" {    
+    cidr_block = "20.0.0.0/16"
     tags = {
         Name = "DEVOPS"
     }
 }
     
-resource "aws_subnet" "devops_public_subnet" 
-{
+resource "aws_subnet" "devops_public_subnet" {
     vpc_id = aws_vpc.devops_vpc.id
-    cidr_block = "20.0.0.0/32"
+    cidr_block = "20.0.0.0/16"
     
     tags = {
         Name = "DEVOPS_subnet"
     }
 }
 
-resource "aws_internet_gateway" "devops_igw" 
-{
+resource "aws_internet_gateway" "devops-igw" {
     vpc_id = aws_vpc.devops_vpc.id    
     tags = {
         Name = "DEVOPS_Internet_Gateway"
@@ -32,10 +42,11 @@ resource "aws_internet_gateway" "devops_igw"
 }
 
 resource "aws_route_table" "public_rt" {
-    vpc_id = aws_vpc.devops_vpc.id  route 
-    {
+    vpc_id = aws_vpc.devops_vpc.id  
+    
+    route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.devops_igw.id
+        gateway_id = aws_internet_gateway.devops-igw.id
     }
 
     tags = {
@@ -43,27 +54,23 @@ resource "aws_route_table" "public_rt" {
     }
 }
 
-resource "aws_route_table_association" "public_subnet_routetable_a" 
-{
+resource "aws_route_table_association" "public_subnet_routetable_a" {
     subnet_id      = aws_subnet.devops_public_subnet.id
     route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_security_group" "web_sg" 
-{
+resource "aws_security_group" "web_sg" {
     name   = "HTTP and SSH"
     vpc_id = aws_vpc.devops_vpc.id  
     
-    ingress 
-    {
+    ingress {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    ingress
-    {
+    ingress {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
@@ -78,51 +85,44 @@ resource "aws_security_group" "web_sg"
     }
 }
 
-resource "aws_key_pair" "ec2_key_pair" 
-{
-    key_name = "assignmentkey"
-    public_key = file("~/.ssh/id_rsa.pub")
-}
 
 
-resource "aws_instance" "web_instance" 
-{  
+
+resource "aws_instance" "web_instance" {  
     instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1e"
 
-    ami = "ami-052387465d846f3fc"
+    ami = "ami-04a81a99f5ec58529"
     subnet_id = aws_subnet.devops_public_subnet.id
 
     vpc_security_group_ids = [aws_security_group.web_sg.id]
     
     associate_public_ip_address = true
     
-    key_name = aws_key_pair.ec2_key_pair.key_name  
+    key_name = aws_key_pair.key_pair.key_name  
+
     tags = {
         Name = "EC2-instance"}
 }
 
-output "internet_gateway_id" 
-{
-    value = aws_internet_gateway.devops-igw.id
+resource "tls_private_key" "rsa_4096"{
+    algorithm = "RSA"
+    rsa_bits = 4096
 }
 
-output "route_table_id" 
-{
-    value = aws_route_table.public_rt.id
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "devops_keypair" #var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-output "security_group_id" 
-{
-    value = aws_security_group.devops_sg.id
+resource "local_file" "private_key" {
+     content = tls_private_key.rsa_4096.private_key_pem
+     filename = "devops_keypair" #var.key_name
+  
 }
-
-output "instance_id" 
-{
-    value = aws_instance.devops-server.id
-}
-
-output "instance_public_ip" 
-{
-    value = aws_instance.devops-server.public_ip
+variable "key_name" {
+  description = "The name of the SSH key pair to use"
+  type        = string
+  default     = "devops_keypair"
 }
